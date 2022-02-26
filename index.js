f9e76055c824d614075c3f143f8f21c176ae2f91
48jorgebrito <jorgeramirobrito19@gmail.com>
2022-02-25 23:00:54 -0300

    menu mobile concluido

---
+++ index.js
@@ -0,0 +1,6 @@
+const btnMobile = document.querySelector(".menu__mobile")
+const menu = document.querySelector(".menu")
+
+btnMobile.addEventListener("click", function(){
+    menu.classList.toggle("active")    
+}) 
